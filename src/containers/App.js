3c289f4e6ab95b6407924c885b7305f28ae786a6
John McCollum <johnvmccollum@gmail.com>
2017-04-26 10:09:39 +0100

    Remove name from State Array once it's been selected

--- src/containers/App.js
+++ src/containers/App.js
@@ -22,8 +22,14 @@ class App extends Component {
 
   start(event){
     event.preventDefault();
+    
     let currentName = _.sample(this.state.names);
+    let newNames = _.remove(this.state.names, name => {
+      return name !== currentName;
+    });
+
     this.setState({
+      names: newNames,
       currentName: currentName
     });
   }
