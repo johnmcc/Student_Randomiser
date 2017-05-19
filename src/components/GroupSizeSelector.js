d9f5afe891085f1d1f882762fea7cf7dc308abb9
Alan Russell <ajrussellaudio@gmail.com>
2017-05-19 14:57:24 +0100

    switch dropdown menu to selectfield with label because UX

--- src/components/GroupSizeSelector.js
+++ src/components/GroupSizeSelector.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import DropDownMenu from 'material-ui/DropDownMenu';
+import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 
 export default ({size, onChange}) => {
     return (
-      <DropDownMenu value={size} onChange={onChange}>
-        <MenuItem value={1} primaryText="1" />
-        <MenuItem value={2} primaryText="2" />
-        <MenuItem value={3} primaryText="3" />
-        <MenuItem value={4} primaryText="4" />
-      </DropDownMenu>
+      <div>
+        <SelectField
+          value={size} 
+          onChange={onChange}
+          floatingLabelText="Size of group:">
+          <MenuItem value={1} primaryText="One" />
+          <MenuItem value={2} primaryText="Pair Programming" />
+          <MenuItem value={3} primaryText="Threesome" />
+          <MenuItem value={4} primaryText="Quartet" />
+        </SelectField>
+      </div>
     );
 }
